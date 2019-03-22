9d5c67d5e78e0d6338811e802b47e5a6d5bf0456
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-03-21 17:46:59 -0700

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log("running");
+
+// 1. We use arguments to call the functions and provide the parameter with a value.
+// 2. Return will end the function and return the value, console.log will display the value within the browsers console.
+// 3. If not returned it will be a undefined value
